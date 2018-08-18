ceda09008730aea7700e5ac5402ea384553b92d8
serinuntius <wasab1gj@gmail.com>
2018-08-18 19:03:05 +0900

    implement percentile and stddev

--- viewer/request.go
+++ viewer/request.go
@@ -4,6 +4,8 @@ import (
 	"encoding/json"
 	"fmt"
 	"io"
+	"math"
+	"sort"
 	"time"
 
 	"github.com/dustin/go-humanize"
@@ -47,10 +49,10 @@ type RequestIndex struct {
 	Min      time.Duration
 	Avg      time.Duration
 	Sum      time.Duration
-	P1       time.Duration
-	P50      time.Duration
-	P99      time.Duration
-	Stddev   time.Duration
+	P1       float64
+	P50      float64
+	P99      float64
+	Stddev   float64
 	Count    int
 	Uri      string
 	Method   string
@@ -69,10 +71,10 @@ func (ri *RequestIndex) String() string {
 		ri.Min.Seconds(),
 		ri.Avg.Seconds(),
 		ri.Sum.Seconds(),
-		ri.P1.Seconds(),
-		ri.P50.Seconds(),
-		ri.P99.Seconds(),
-		ri.Stddev.Seconds(),
+		ri.P1,
+		ri.P50,
+		ri.P99,
+		ri.Stddev,
 		humanize.Bytes(ri.MaxBody),
 		humanize.Bytes(ri.MinBody),
 		humanize.Bytes(ri.AvgBody),
@@ -104,7 +106,6 @@ func (rp *RequestParser) Parse() error {
 		if err != nil {
 			return errors.Wrap(err, "Failed to parse time")
 		}
-
 
 		path := r.Path
 
@@ -161,6 +162,26 @@ func (rp *RequestParser) Parse() error {
 
 	idx := 0
 	for _, ri := range rm {
+		// averages
+		ri.Avg = ri.Sum / time.Duration(ri.Count)
+		ri.AvgBody = ri.SumBody / uint64(ri.Count)
+
+		// Percentile
+		// Need sort
+		sort.Slice(ri.Requests, func(i, j int) bool {
+			return ri.Requests[i].Time < ri.Requests[j].Time
+		})
+
+		p1Len := lenPercentile(len(ri.Requests), 1)
+		ri.P1 = ri.Requests[p1Len].Time
+		p50Len := lenPercentile(len(ri.Requests), 50)
+		ri.P50 = ri.Requests[p50Len].Time
+
+		p99Len := lenPercentile(len(ri.Requests), 99)
+		ri.P99 = ri.Requests[p99Len].Time
+
+		ri.Stddev = requestTimeStddev(ri.Requests, ri.Avg.Seconds())
+
 		ris[idx] = *ri
 		idx++
 	}
@@ -169,5 +190,24 @@ func (rp *RequestParser) Parse() error {
 
 	// TODO avg,p,stddev系は後で数える
 	return nil
+
+}
+
+func lenPercentile(l int, n int) (pLen int) {
+	pLen = (l * n / 100) - 1
+	if pLen < 0 {
+		pLen = 0
+	}
+
+	return pLen
+}
+
+func requestTimeStddev(requests []RequestMinimum, avg float64) (stddev float64) {
+	n := float64(len(requests))
+	for _, r := range requests {
+		stddev += (r.Time - avg) * (r.Time - avg)
+	}
+
+	return math.Sqrt(stddev / n)
 
 }
